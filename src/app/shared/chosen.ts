fbe48a907894e00c28092f814aa1fd3e477539fe
Tin Pavlic <tinpavlic@gmail.com>
2018-05-08 22:42:51 +0200

    Prvi dio teskog dijela gotov u Zadatku 1

---
+++ src/app/shared/chosen.ts
@@ -0,0 +1,13 @@
+export class Chosen {
+  name: string;
+  image: string;
+  category: string;
+  label: string;
+  price: string;
+  description: string;
+  comments: string;
+  rating: number;
+  comment: string;
+  author: string;
+  date: string;
+}
