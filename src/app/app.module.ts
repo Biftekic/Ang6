cf5c627d51bf02abe6c0852dcb16e61191db5f87
Tin Pavlic <tinpavlic@gmail.com>
2018-05-13 22:57:44 +0200

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,12 +12,16 @@ import { MenuComponent } from './menu/menu.component';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent
 
 
   ],
