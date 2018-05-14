9577a24e38f90e2f2735625f759ed0dcebb15eed
Tin Pavlic <tinpavlic@gmail.com>
2018-05-14 22:22:17 +0200

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
 
 @NgModule({
   declarations: [
@@ -45,7 +46,7 @@ import { DishService } from './services/dish.service';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
